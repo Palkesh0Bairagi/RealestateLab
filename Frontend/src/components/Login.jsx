9c85bdb783999ded0f2cee3ada6357e4d31a4255
Palkesh0Bairagi <137200059+Palkesh0Bairagi@users.noreply.github.com>
2025-01-24 16:22:43 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("https://realestatelab-cmyb.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
